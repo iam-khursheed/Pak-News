24ba4ea6d549ef031490a519b8af31c82b058b45
iam-khursheed <iam.khursheed@outlook.com>
2025-11-17 14:07:58 +0500

    news

---
+++ components/icons/TranslateIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TranslateIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 5h12M9 3v2m4 13l4-4M19 17v-2a4 4 0 00-4-4H9a4 4 0 00-4 4v2m14-8l-4-4m0 0l-4 4m4-4v12" />
+    </svg>
+);
+
+export default TranslateIcon;
---
+++ constants.ts
@@ -0,0 +1,3 @@
+import { NewsCategory } from './types';
+
+export const CATEGORIES: NewsCategory[] = ['All', 'Politics', 'World', 'Technology', 'Business', 'Sports', 'Offline'];
